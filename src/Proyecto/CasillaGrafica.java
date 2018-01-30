031da5a9f75a81badc96fa3ef15b25ad16365623
SamuelVera <samuelvera1999@gmail.com>
2018-01-30 10:20:24 -0400

    Clase de la parte gráfica de la casilla

--- src/Proyecto/CasillaGrafica.java
+++ src/Proyecto/CasillaGrafica.java
@@ -11,84 +11,201 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
- * @author Samuel Vera
- * CI: 26475783
- * Carnet: 20171110080
+ * Clase con la cual se crea la interfaz gráfica de cada casilla, además que
+ * se implementa el evento de click de la interfaz ActionListener para que el
+ * usuario interactue al clickear sobre la casilla
  */
 
 public class CasillaGrafica extends JButton implements ActionListener{
-    private int i,j;
+    private int i,j; //Coordenadas de la casilla
+        //Tablero con objetos incializados en la clase MenuPrincipal
     private Casilla[][] tablero = MenuPrincipal.getTablero();
+        //Tablero grafico con objetos inicializados en la clase Tablero
     private CasillaGrafica[][] tablero2 = Tablero.getTabDinamico();
     
         //Constructor de los parámetros gráficos de la casilla
     CasillaGrafica(int x, int y, int alto, int ancho){
         setLocation(x, y);//Parámetros posición en y, en x
-        setPreferredSize(new Dimension(ancho,alto));
+        setPreferredSize(new Dimension(ancho,alto)); //Tamaño de la casilla
         addActionListener(this); //Implementación del Action Listener para evento de click
         this.setBackground(Color.gray);
+        setCoord(x,y);
     }
-    public void setCoord(int i, int j){    //Coordenada de la casilla
+    public void setCoord(int j, int i){ //Establecer coordenada de la casilla
         this.i = i;
         this.j = j;
     }
 
-    @Override //Implementación del Action Listener para evento de click
+        /*Implementación del método actionPerformed Action Listener
+          para el evento del click*/
+    @Override 
     public void actionPerformed(ActionEvent e) {
         int aux;
         JFrame frame = new JFrame(); //Ventana de selección con opciones personalizadas
-        String[] opcion = new String[2];
+        String[] opcion = new String[2]; //Arreglo con opciones para la ventana
         opcion[0] = "Mostrar";
         opcion[1] = "Marcar";
+            /*Se almacena el valor del indice del arreglo que va acorde a la opción
+              seleccionada en la variable auxiliar*/
         aux = JOptionPane.showOptionDialog(frame.getContentPane(),"CASILLA("+this.i+","+this.j+")","¿Mostrar o Marcar?",0,JOptionPane.INFORMATION_MESSAGE,null,opcion,null);
-            //La variable auxiliar se vuelve igual al índice de lo seleccionado en el arreglo del tipo String
-        if(aux == 1){ //Opción de marcar casilla
+        if(aux == 1){ //Opción de marcar casilla selecionada
             opcion[0] = "Bandera";
             opcion[1] = "Desconocido";
+            /*Se almacena en la variable auxiliar el valor del indice que va a 
+              corresponder con la opción seleccionada*/
             aux = JOptionPane.showOptionDialog(frame.getContentPane(),"Selecciona un tipo de marca","",0,JOptionPane.INFORMATION_MESSAGE,null,opcion,null);
             if(aux == 0){ //Poner icono de la bandera en el botón
-                setImagen("src\\Imagenes\\Bandera_Amarilla.png",0,0,false);
+                setImagen("src\\Imagenes\\Bandera_Amarilla.png");
             }else if(aux == 1){ //Poner icono de interrogación en el botón
-                setImagen("src\\Imagenes\\Misterio.png",0,0,false);
+                setImagen("src\\Imagenes\\Misterio.png");
             }
-        }else if(aux == 0){ //Opción de mostrar la casilla
+        }else if(aux == 0){ //Opción de mostrar la casilla selecionada
             determinarCasilla();
         }
     }
-        //Determinar contenido de la casilla
+        //Método en el que se determina el contenido de la casilla
     private void determinarCasilla(){
-        if(tablero[this.i][this.j].isVacio() == false){
+        if(tablero[this.i][this.j].isVacio() == false){ //La casilla contiene una mina
             perder();
-        }else if(tablero[this.i][this.j].isVacio() == true){
-            buscar();
+        }else if(tablero[this.i][this.j].isVacio() == true){ //La casilla esta vacia
+            int m=this.i, n=this.j;
+            buscar(m,n);
+            ganar();
         }
     }
-        //Buscar casillas de los alrededores
-    private void buscar(){
-        
+    /*En el método "buscar" se implementa recursividad para hallar el contenido de
+      las casillas adyacentes. Se contemplan los siguientes casos en el mismo:
+        **Se aplica recursividad**
+            -Si las 8 casillas adyacentes no poseen mina, además se procede a evaluar
+            esas mismas 8 casillas en busqueda de minas.
+        **Casos base para detener la recursividad**
+            -Si el índice no existe en los límites de la matriz
+            -Si la casilla ya ha sido visitada
+            -Si alguna casilla adyacente posee una bomba
+    */
+    private int buscar(int m, int n){
+        if(m>=0 && n>=0 && m<tablero.length && n<tablero.length){ //Buscar si el índice existe
+          if(tablero[m][n].isVisual()==false){ //Determinar si la casilla ya ha sido visitada
+            int nBombas;
+            nBombas = 0;
+              //Barrido de casillas adyacentes en búsqueda de bombas
+            for (int k=m-1;k<m+2;k++){
+              for (int l=n-1;l<n+2;l++){
+                 //Evitar excepciones donde se evalue un indice mayor al del arreglo o negativo
+                if(k>=0 && l>=0 && (k!=m || l!=n) && k<tablero.length && l<tablero.length){
+                  if(tablero[k][l].isVisual() == false && tablero[k][l].isVacio() == false){
+                    nBombas++;
+                  }
+                }
+              }
+            }
+            tablero[m][n].setVisual(true);
+            tablero2[m][n].setEnabled(false);
+            if(nBombas == 0){        
+              buscar(m-1,n-1); //Arriba a la izquierda
+              buscar(m-1,n);   //Arriba
+              buscar(m-1,n+1); //Arriba a la derecha
+              buscar(m,n-1);   //Izquierda
+              buscar(m,n+1);   //Derecha
+              buscar(m+1,n-1); //Abajo a la izquierda
+              buscar(m+1,n);   //Abajo
+              buscar(m+1,n+1); //Abajo a la derecha
+            }else{
+              numero(nBombas,m,n);
+              return 0; //Caso en el que hay una bomba adyacente
+            }
+          }else {
+            return 0; //Caso de la casilla ya ha sido visitada
+          }
+        }else{
+            return 0; //Caso de índice inexistente
+        }
+        return 0;
     }
-        //Pérdida de partida
+        //Método para seleccionar el número que se le colocará a la casilla
+    private void numero(int Bombas, int m, int n){
+        switch (Bombas) {
+            case 1:
+                setImagen("src\\Imagenes\\Uno.png",m,n);
+                break;
+            case 2:
+                setImagen("src\\Imagenes\\Dos.png",m,n);
+                break;
+            case 3:
+                setImagen("src\\Imagenes\\Tres.png",m,n);
+                break;
+            case 4:
+                setImagen("src\\Imagenes\\Cuatro.png",m,n);
+                break;
+            case 5:
+                setImagen("src\\Imagenes\\Cinco.png",m,n);
+                break;
+            case 6:
+                setImagen("src\\Imagenes\\Seis.png",m,n);
+                break;
+            case 7:
+                setImagen("src\\Imagenes\\Siete.png",m,n);
+                break;
+            case 8:
+                setImagen("src\\Imagenes\\Ocho.png",m,n);
+                break;
+            default:
+                break;
+        }
+    }
+        //Método de pérdida de la partida
     private void perder(){
-        for(int k=0;k<tablero.length;k++){
+        setImagen("src\\Imagenes\\Mina.png");
+        this.setBackground(Color.red);
+        for(int k=0;k<tablero.length;k++){ //Barrido del arreglo para revelar las minas
             for(int l=0;l<tablero.length;l++){
                 if(tablero[k][l].isVacio() == false){
                     tablero2[k][l].setBackground(Color.red);
-                    setImagen("src\\Imagenes\\Mina.png",k,l,true);
+                    setImagen("src\\Imagenes\\Mina.png",k,l);
                 }
             }
         }
+        GameOver over = new GameOver();
+        over.setVisible(true);
     }
-        //Colocar o cambiar imagen de la casilla
-    private void setImagen(String a, int b, int c, boolean d){
+        //Método para determinar si hay condición de victoria
+    private void ganar(){
+        int aux=0, aux2=0;
+        for(int k=0;k<tablero.length;k++){
+            for(int l=0;l<tablero.length;l++){
+                if(tablero[k][l].isVisual()==true){
+                    aux++;
+                }
+                if(tablero[k][l].isVacio()==false)
+                {
+                    aux2++;
+                }
+            }
+        }
+            //Condición de victoria
+        if(aux == (tablero.length*tablero.length)-aux2){
+            GameOver over = new GameOver(1);
+            over.setVisible(true);
+        }
+    }
+        //Método para colocar o cambiar imagen de la casilla
+        //Parametros: Dirección e indices
+    private void setImagen(String a, int b, int c){
         ImageIcon img = new ImageIcon(a);
         Image image = img.getImage(); //Transformación de ImageIcon a Image
         //Escalado de la imagen acorde a las dimensiones de la casilla
         Image newimg = image.getScaledInstance(this.getHeight(), this.getWidth(),  java.awt.Image.SCALE_SMOOTH);
         img = new ImageIcon(newimg); //Transformación de vuelta a ImageIcon
-        if(d == false){
-            this.setIcon(img);
-        }else if(d == true){
-            tablero2[b][c].setIcon(img);
-        }
+        tablero2[b][c].setIcon(img); //Colocar la imagen
+    }
+        //Sobrecarga del metodo setImage
+        //Parametros Dirección
+    private void setImagen(String a){
+        ImageIcon img = new ImageIcon(a);
+        Image image = img.getImage(); //Transformación de ImageIcon a Image
+        //Escalado de la imagen acorde a las dimensiones de la casilla
+        Image newimg = image.getScaledInstance(this.getHeight(), this.getWidth(),  java.awt.Image.SCALE_SMOOTH);
+        img = new ImageIcon(newimg); //Transformación de vuelta a ImageIcon
+        this.setIcon(img); //Colocar la imagen
     }
 }
