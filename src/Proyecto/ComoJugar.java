8fa8a9064736262a9cf36b03e40859ce562fcb21
SamuelVera <samuelvera1999@gmail.com>
2018-02-11 19:18:38 -0400

    Vetana de instrucciones del juego

--- src/Proyecto/ComoJugar.java
+++ src/Proyecto/ComoJugar.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Proyecto;
 
-/**
- *
- * @author samue
- */
 public class ComoJugar extends javax.swing.JFrame {
 
     /**
-     * Constructor psts Jform ComoJugar
+     * Constructor para Jform ComoJugar
      */
     public ComoJugar() {
         initComponents();
