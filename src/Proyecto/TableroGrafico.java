1cda273b7fef329cd17edc5522acbf5666096541
SamuelVera <samuelvera1999@gmail.com>
2018-02-12 22:12:25 -0400

    Ventana del despliegue del tablero

--- src/Proyecto/TableroGrafico.java
+++ src/Proyecto/TableroGrafico.java
@@ -17,6 +17,8 @@ public class TableroGrafico extends javax.swing.JFrame {
         this.lado = lado;
         this.nminas = nminas;
         this.setLocationRelativeTo(null);
+        this.dificultad.setText("Minas:" +this.nminas);
+        this.ID.setText("ID: "+MenuPrincipal.ID2);
     }
 
     /**
@@ -30,8 +32,9 @@ public class TableroGrafico extends javax.swing.JFrame {
         tableroD = new javax.swing.JPanel();
         dificultad = new javax.swing.JLabel();
         mostrar = new javax.swing.JToggleButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        hud = new javax.swing.JTextPane();
+        dificultad1 = new javax.swing.JLabel();
+        ID = new javax.swing.JLabel();
+        ID1 = new javax.swing.JLabel();
         fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -46,8 +49,9 @@ public class TableroGrafico extends javax.swing.JFrame {
         tableroD.setVerifyInputWhenFocusTarget(false);
         getContentPane().add(tableroD, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        dificultad.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        getContentPane().add(dificultad, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 90, 10));
+        dificultad.setFont(new java.awt.Font("Tahoma", 0, 22));
+        dificultad.setText("");
+        getContentPane().add(dificultad, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 500, 140, 70));
 
         mostrar.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         mostrar.setText("Mostrar Mapa");
@@ -58,19 +62,19 @@ public class TableroGrafico extends javax.swing.JFrame {
         });
         getContentPane().add(mostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 340, -1, -1));
 
-        hud.setEditable(false);
-        hud.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        hud.setText("Minas: "+MenuPrincipal.nminas);
-        hud.setAutoscrolls(false);
-        hud.setFocusable(false);
-        hud.setOpaque(false);
-        hud.setRequestFocusEnabled(false);
-        jScrollPane1.setViewportView(hud);
+        dificultad1.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        dificultad1.setText("");
+        getContentPane().add(dificultad1, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 480, 140, 70));
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 630, 280, 40));
+        ID.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        getContentPane().add(ID, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 470, 220, 40));
 
-        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo_mapa_topografia.png"))); // NOI18N
-        getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 920, 800));
+        ID1.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        getContentPane().add(ID1, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 450, 170, 20));
+
+        fondo.setForeground(new java.awt.Color(255, 255, 255));
+        fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Fondo_Mapa_1.jpg"))); // NOI18N
+        getContentPane().add(fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -110, 960, 1140));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -78,7 +82,8 @@ public class TableroGrafico extends javax.swing.JFrame {
         //Declaración del arreglo de las gráficas de la casilla
     private static CasillaGrafica[][] tabDinamico;
     private static Casilla[][] casillas;
-    private int lado, nminas;
+    private final int lado, nminas;
+    
         //Getter para los arreglos de los tableros
     public static CasillaGrafica[][] getTabDinamico(){
         return tabDinamico;
@@ -89,43 +94,42 @@ public class TableroGrafico extends javax.swing.JFrame {
     }
         //Método para construir la interfaz tablero dependiente de la dificultad
     private void construir(int lado, int nminas){
-        int tamaño1 = 0, tamaño2 = 0;
+        int tamaño = 0;
         this.setLocationRelativeTo(null);
-        this.setSize(800,770);
-        tableroD.setPreferredSize(new Dimension(800,770));
+        this.tableroD.setPreferredSize(new Dimension(800,770));
         if(lado == 10 && nminas == 10){ //Dificultad Facil
             this.setSize(900,750);
-            tableroD.setPreferredSize(new Dimension(750,750));
-            tamaño1 = 65;
-            tamaño2 = 65;
+            this.tableroD.setPreferredSize(new Dimension(750,750));
+            tamaño = 65;
         }else if(lado == 15 && nminas == 40){   //Dificultad Media
             this.setSize(900,750);
-            tableroD.setPreferredSize(new Dimension(750,750));
-            tamaño1 = 43;
-            tamaño2 = 43;
+            this.tableroD.setPreferredSize(new Dimension(750,750));
+            tamaño = 43;
         }else if(lado == 22 && nminas == 100){  //Dificultad Dificil
-            tamaño1 = 27;
-            tamaño2 = 30;
+            tamaño = 30;
         }else{  //Dificultad Personalizada
-            tamaño1 = Personalizar.getTamaño1();
-            tamaño2 = Personalizar.getTamaño1();
+            tamaño = Personalizar.getTamaño();
         }
-        dificultad.setText("Minas:" +nminas);
+        this.dificultad.setText("");
+        this.ID.setText("");
+        this.dificultad1.setText("Minas: "+nminas);
+        this.ID1.setText("ID: "+MenuPrincipal.ID2);
             //El arreglo para las gráficas debe ser del mismo tamaño que el contruido en MenuPrincipal
-        tabDinamico = new CasillaGrafica[lado][lado];
-        for(int i=0;i<tabDinamico.length;i++){
-            for(int j=0;j<tabDinamico.length;j++){
+        this.tabDinamico = new CasillaGrafica[lado][lado];
+        for(int i=0;i<this.tabDinamico.length;i++){
+            for(int j=0;j<this.tabDinamico.length;j++){
                      //Construcción del objeto de la Casilla Gráfica
-                tabDinamico[i][j] = new CasillaGrafica(j,i,tamaño1,tamaño2);
+                this.tabDinamico[i][j] = new CasillaGrafica(j,i,tamaño,tamaño);
                     //Añadir la casilla al tablero
-                tableroD.add(tabDinamico[i][j]);
+                this.tableroD.add(this.tabDinamico[i][j]);
             }
+       
         }
-        hud.setLocation(450, 1000);
     }
-        //
+    
+        //Método para repartir los contenidos de las casillas
     protected void colocarCasillas(int lado, int limiteMinas){
-        casillas = new Casilla[lado][lado];
+        this.casillas = new Casilla[lado][lado];
         int minaux=0, rand;
         Random random = new Random();
         /*Barrido del arreglo y repartición de minas de manera aleatoria
@@ -133,13 +137,13 @@ public class TableroGrafico extends javax.swing.JFrame {
         varias veces el barrido, solo que en la siguientes veces que se repita
         ignorará las casillas que ya guardan una mina*/
         while(minaux < limiteMinas){
-            for(int i=0;i<casillas.length;i++){
-                for(int j=0;j<casillas.length;j++){
+            for(int i=0;i<this.casillas.length;i++){
+                for(int j=0;j<this.casillas.length;j++){
                     rand = random.nextInt(25);
-                    if(casillas[i][j] == null){
-                        casillas[i][j] = new Vacio(true); //Objeto de vacio
-                    }else if(rand >= 23 && minaux < limiteMinas && casillas[i][j].vacio!=false){
-                        casillas[i][j] = new Mina(false); //Objeto de mina
+                    if(this.casillas[i][j] == null){
+                        this.casillas[i][j] = new Vacio(true); //Objeto de vacio
+                    }else if(rand >= 23 && minaux < limiteMinas && this.casillas[i][j].vacio!=false){
+                        this.casillas[i][j] = new Mina(false); //Objeto de mina
                         minaux++;
                     }
                 }
@@ -150,15 +154,16 @@ public class TableroGrafico extends javax.swing.JFrame {
     private void mostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mostrarActionPerformed
         colocarCasillas(this.lado,this.nminas);
         construir(this.lado,this.nminas);
-        mostrar.setEnabled(false);
-        mostrar.setVisible(false);
+        this.mostrar.setEnabled(false);
+        this.mostrar.setVisible(false);
     }//GEN-LAST:event_mostrarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel ID;
+    private javax.swing.JLabel ID1;
     private javax.swing.JLabel dificultad;
+    private javax.swing.JLabel dificultad1;
     private javax.swing.JLabel fondo;
-    private javax.swing.JTextPane hud;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JToggleButton mostrar;
     private javax.swing.JPanel tableroD;
     // End of variables declaration//GEN-END:variables
