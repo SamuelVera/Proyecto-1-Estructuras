617d96c3b972b5bfe92030d0689622fdc2308e7e
SamuelVera <samuelvera1999@gmail.com>
2018-02-17 00:03:05 -0400

    Clase Jugador

---
+++ src/Proyecto/Jugador.java
@@ -0,0 +1,13 @@
+package Proyecto;
+
+public class Jugador {
+    private static String id;
+    
+    public static void setID(String id2){
+        id = id2;
+    }
+    
+    public static String getId(){
+        return id;
+    }
+}
